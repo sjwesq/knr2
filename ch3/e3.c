62c0f8ecccd3c7de7ca1bf6c619e801484712014
sjwesq <sophie@sjwesq.dev>
2025-10-04 19:38:13 -0400

    ch3/e3: converted to using ctype.h

--- ch3/e3.c
+++ ch3/e3.c
@@ -3,15 +3,13 @@
 // letters of either case and digits, and be prepared to handle cases like a-b-c
 // and a-z0-9 and -a-z . Arrange that a leading or trailing '-' is taken
 // literally.
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 static void Expand(const char str1[], char str2[]);
 bool CheckValidRange(char c1, char c2);
-static bool IsUppercase(char c);
-static bool IsLowercase(char c);
-static bool IsNumber(char c);
 static void TestExpand(const char str[]);
 
 #define MAX_STR_LENGTH 256
@@ -34,7 +32,7 @@ static void Expand(const char str1[], char str2[]) {
   unsigned out_pos = 0;
   for (int i = 0; str1[i] != '\0'; ++i) {
     char c_now = str1[i];
-    if (IsUppercase(c_now) || IsLowercase(c_now) || IsNumber(c_now)) {
+    if (isupper(c_now) || islower(c_now) || isdigit(c_now)) {
       if (CheckValidRange(last_alphanumeric, c_now) && range_enabled) {
         for (int j = 1; j <= c_now - last_alphanumeric; ++j) {
           str2[out_pos++] = last_alphanumeric + (char)j;
@@ -54,34 +52,13 @@ static void Expand(const char str1[], char str2[]) {
 // c1 is expected to be the lesser value
 bool CheckValidRange(char c1, char c2) {
   if (c1 < c2) {
-    if (IsUppercase(c1) && IsUppercase(c2)) {
+    if (isupper(c1) && isupper(c2)) {
       return true;
-    } else if (IsLowercase(c1) && IsLowercase(c2)) {
+    } else if (islower(c1) && islower(c2)) {
       return true;
-    } else if (IsNumber(c1) && IsNumber(c2)) {
+    } else if (isdigit(c1) && isdigit(c2)) {
       return true;
     }
-  }
-  return false;
-}
-
-static bool IsUppercase(char c) {
-  if (c >= 'A' && c <= 'Z') {
-    return true;
-  }
-  return false;
-}
-
-static bool IsLowercase(char c) {
-  if (c >= 'a' && c <= 'z') {
-    return true;
-  }
-  return false;
-}
-
-static bool IsNumber(char c) {
-  if (c >= '0' && c <= '9') {
-    return true;
   }
   return false;
 }
